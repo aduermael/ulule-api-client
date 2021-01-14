55f073e8ade39a6ea99fd75a4002565e08b4d85f
Adrien Duermael <adrien@duermael.com>
2021-01-14 21:23:45 +0100

    add GetUserOrders

--- user.go
+++ user.go
@@ -1,6 +1,9 @@
 package ulule
 
-import "strconv"
+import (
+	"errors"
+	"strconv"
+)
 
 // GetUser returns a User for for given ID
 func (c *Client) GetUser(userID int) (*User, error) {
@@ -22,4 +25,27 @@ func (c *Client) Me() (*User, error) {
 		return nil, err
 	}
 	return resp, nil
+}
+
+// GetUserOrders lists orders for a user
+// limit and offset stand for pagination
+// the boolean returns indicates if it was the last
+// page or not.
+// This function only works for connected user.
+func (c *Client) GetUserOrders(u *User, limit, offset int) ([]*Order, error, bool) {
+	if u == nil {
+		return nil, errors.New("user can't be nil"), false
+	}
+
+	userIDStr := strconv.Itoa(u.ID)
+	limitStr := strconv.Itoa(limit)
+	offsetStr := strconv.Itoa(offset)
+
+	orders := &ListOrderResponse{}
+	err := c.apiget("/users/"+userIDStr+"/orders?limit="+limitStr+"&offset="+offsetStr, orders)
+	if err != nil {
+		return nil, err, false
+	}
+
+	return orders.Orders, nil, orders.Meta.Next == ""
 }
